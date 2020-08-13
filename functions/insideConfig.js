8812c104c9493e7dcc43335d1dbcfe276e81abd2
glab_16 <glabstrizhkov@gmail.com>
2020-08-13 11:21:16 +0300

    1.0.1

--- functions/insideConfig.js
+++ functions/insideConfig.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 
 exports.connect = (dirname, path) => {
     let fullPath = pathLib.join(dirname, path);
-    fs.writeFileSync('./config/config.txt', fullPath);
+    fs.writeFileSync(pathLib.join(__dirname, '../config/config.txt'), fullPath);
 }
 
 exports.get = () => {
-    let fullPath = fs.readFileSync('./config/config.txt', 'utf8');
+    let fullPath = fs.readFileSync(pathLib.join(__dirname, '../config/config.txt'), 'utf8');
     return fullPath;
 }
